69ec70182ccef5a4e87e3bde443450d20c87e816
Andrew McInally <realfunnyestate@yahoo.com>
2021-11-17 23:22:34 -0700

    last one

--- script.js
+++ script.js
@@ -145,5 +145,11 @@ const data = [
 ];
 
 const transportation = data.reduce(function (obj, item) {
-  console.log(item);
+  if (!obj[item]) {
+    obj[item] = 0;
+  }
+  obj[item]++;
+  return obj;
 });
+
+console.log(transportation);
